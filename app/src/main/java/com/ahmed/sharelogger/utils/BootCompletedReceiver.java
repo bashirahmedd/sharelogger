083abc87712c91cfb7621f6639ec24e008f1e4e4
bashir <bashir.ahmedd@gmail.com>
2022-02-10 01:25:17 +0500

    service and reciever added

---
+++ app/src/main/java/com/ahmed/sharelogger/utils/BootCompletedReceiver.java
@@ -0,0 +1,21 @@
+package com.ahmed.sharelogger.utils;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+//import android.support.v4.content.ContextCompat;
+import androidx.core.content.ContextCompat;
+
+public class BootCompletedReceiver extends BroadcastReceiver {
+
+    @Override public void onReceive(Context context, Intent intent) {
+        if (intent.getAction().equals(Intent.ACTION_BOOT_COMPLETED)
+                && SettingsManager.getInstance(context).getClipboardMonitorServiceEnabled()) {
+            Intent updateServiceNotificationIntent = new Intent(context, ClipboardMonitorService.class);
+            updateServiceNotificationIntent.setAction(Constants.CustomAction.UPDATE_SERVICE_NOTIFICATION);
+            ContextCompat.startForegroundService(context, updateServiceNotificationIntent);
+        }
+    }
+
+}
